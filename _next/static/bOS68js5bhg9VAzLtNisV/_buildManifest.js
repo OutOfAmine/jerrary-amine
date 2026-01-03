35f5a49c2b3664fe8ab7ece5d2f064a357c311af
Amine <jerrary.contact@gmail.com>
2026-01-03 17:59:41 +0100

    Updates

--- _next/static/bOS68js5bhg9VAzLtNisV/_buildManifest.js
+++ _next/static/bOS68js5bhg9VAzLtNisV/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/jerrary-amine//_next/:path+",
-        "destination": "/jerrary-amine/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
